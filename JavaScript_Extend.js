be912726a26aaa5e00e10ab434f0a97d3531dce8
Warden <zw9409@126.com>
2017-11-17 23:11:01 +0800

    0.1

--- JavaScript_Extend.js
+++ JavaScript_Extend.js
@@ -4,7 +4,12 @@
 /*                           */
 /*****************************/
 
-//计算yyyy-mm-dd型日期的天数差 
+
+/**
+ * 计算yyyy-mm-dd型日期的天数差
+ * @param {String} s1 yyyy-mm-dd
+ * @param {String} s2 yyyy-mm-dd
+ */
 Date.prototype.getDateDifday = function (s1, s2) {
     var d1 = new Date(Date.parse(s1.replace(/-/g, "/")));
     var d2 = new Date(Date.parse(s2.replace(/-/g, "/")));
@@ -12,7 +17,11 @@ Date.prototype.getDateDifday = function (s1, s2) {
     var r = Math.floor(dd / (1000 * 60 * 60 * 24));
     return r;
 }
-// 格式化
+
+/**
+ * 格式化Date对象为 yyyy-mm-dd
+ * @param {String} d 连接符，默认为中划线
+ */
 Date.prototype.format = function (d) {
     if (!d) { d = "-"; }
     var oDate = this;
@@ -27,12 +36,17 @@ Date.prototype.format = function (d) {
     var result = oDate.getFullYear() + d + iMonth + d + iDate;
     return result;
 }
-//改变日期
+
+/**
+ * 根据时间间隔，获取一个Date
+ * @param {String} strInterval 单位:s秒 m分 h时 d天 w星期 q季度 m月 y年
+ * @param {String} Number 数
+ */
 Date.prototype.dateAdd = function (strInterval, Number) {
     var dtTmp = this;
     switch (strInterval) {
         case 's': return new Date(Date.parse(dtTmp) + (1000 * Number));
-        case 'n': return new Date(Date.parse(dtTmp) + (60000 * Number));
+        case 'm': return new Date(Date.parse(dtTmp) + (60000 * Number));
         case 'h': return new Date(Date.parse(dtTmp) + (3600000 * Number));
         case 'd': return new Date(Date.parse(dtTmp) + (86400000 * Number));
         case 'w': return new Date(Date.parse(dtTmp) + ((86400000 * 7) * Number));
