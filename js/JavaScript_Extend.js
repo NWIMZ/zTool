529b71cfc71eaabd1dc01e234dcdbb6c8f67c16e
zw9409 <zw9409@126.com>
2017-12-06 18:57:09 +0800

    1

--- js/JavaScript_Extend.js
+++ js/JavaScript_Extend.js
@@ -80,10 +80,24 @@ Array.prototype.indexOf = function (item) {
 }
 /**
  * 删除数组中的指定元素
- * @param {*} item 数组元素，通常只能为基本数据类型
+ * @param {*} item 数组元素或者下标
  */
-Array.prototype.delete = function (item) {
-    var index = this.indexOf(item);
+Array.prototype.delete = function (item, type) {
+    var index;
+    // 没有指定类型
+    if (!type) {
+        if (typeof item == "number") {
+            index = item;
+        } else {
+            index = this.indexOf(item)
+        }
+    } else {
+        if (type == "index") {
+            index = item;
+        } else if (type == "item") {
+            index = this.indexOf(item)
+        }
+    }
     if (index != -1) {
         this.splice(index, 1);
     }
