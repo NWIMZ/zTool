78205ce0fce3b9b971928b7006e93ea611b602bc
zw9409 <zw9409@126.com>
2018-03-20 16:16:30 +0800

    1

---
+++ js/pubsub.js
@@ -0,0 +1,35 @@
+// 发布/订阅器
+var pubsub = (function(){
+	var q = {};
+	var topics = {};
+	var subId = -1;
+	/**
+	 * 发布
+	 * @param {String} topic 发布的消息名
+	 * @param {*} args 参数（消息内容）
+	 */
+	q.publish = function(topic,args){
+		if(!topics[topic]){return;}
+		var subs = topics[topic];
+		var len = subs.length;
+		while(len--){
+			subs[len].func(args);
+		}
+		return this;
+	};
+	/**
+	 * 订阅
+	 * @param {*} topic 订阅的消息名
+	 * @param {*} func 收到消息后的回调函数
+	 */
+	q.subscribe = function(topic,func){
+		topics[topic] = topics[topic] || [];
+		var _token = (++subId).toString();
+		topics[topic].push({
+			token: _token,
+			func:func
+		});
+		return _token;
+	};
+	return q;
+})();
