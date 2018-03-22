f1ebf9ddf35fd7bdafe160b6725b4f1a8dbac8a3
zw9409 <zw9409@126.com>
2018-03-22 17:49:21 +0800

    1

--- js/Extend_Array.js
+++ js/Extend_Array.js
@@ -9,7 +9,7 @@
  * 查找数组中的指定元素，返回下标，没有则返回-1
  * @param {*} item 数组元素，通常只能为基本数据类型
  */
-Array.prototype.indexOf = function (item) {
+Array.prototype.indexOf = function(item) {
     var index = -1;
     for (var i = 0; i < this.length; i++) {
         if (this[i] === item) {
@@ -18,29 +18,30 @@ Array.prototype.indexOf = function (item) {
         }
     }
     return index;
-}
+};
+
 /**
  * 删除数组中的指定元素
  * @param {*} item 数组元素或者下标
  */
-Array.prototype.delete = function (item, type) {
+Array.prototype.delete = function(item, type) {
     var index;
     // 没有指定类型
     if (!type) {
         if (typeof item == "number") {
             index = item;
         } else {
-            index = this.indexOf(item)
+            index = this.indexOf(item);
         }
     } else {
         if (type == "index") {
             index = item;
         } else if (type == "item") {
-            index = this.indexOf(item)
+            index = this.indexOf(item);
         }
     }
     if (index != -1) {
         this.splice(index, 1);
     }
     return this;
-}
+};
